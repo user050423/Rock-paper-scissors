7c2a228021e852ce364954bcdc8f3f10afc6db70
Ynotkokonut <naijabami@gmail.com>
2026-03-09 12:11:52 +0800

    Use for loop to play the game 5 times
    Keep track of scores while looping every round

--- script.js
+++ script.js
@@ -9,25 +9,43 @@ function getComputerChoice() {
     } 
 }
 
-const computer = getComputerChoice();
-
 function getPlayerChoice() {
     const player = prompt("Rock, Paper, or Scissors?").toLowerCase();
     return player;
 }
 
-const player = getPlayerChoice();
 
-function play(player, computer) {
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(player, computer) {
     if (player === computer) {
         return "It's a tie!";
     } else if (player === "rock" && computer === "scissors" || player === "paper" && computer === "rock" || player === "scissors" && computer === "paper") {
-        return "You win!";
+        playerScore++;      
+        return `You win! ${player} beats ${computer}`;
     } else {
-        return "You lose!";
+        computerScore++;
+        return `You lose! ${computer} beats ${player}`;
     }
 }
 
-const result = play(player, computer);
-console.log(result);
+function playGame() {
+   
+    for (let i = 0; i < 5; i++) {
+        const computer = getComputerChoice();
+        const player = getPlayerChoice();
 
+        console.log(playRound(player, computer));
+        console.log(`Score: Player ${playerScore} - Computer ${computerScore}`);
+    }
+
+    if (playerScore === 5) {
+        console.log("Congratulations! You won the game!");  
+    } else {
+        console.log("Sorry, you lost the game. Better luck next time!");    
+    }
+}
+
+playGame();
